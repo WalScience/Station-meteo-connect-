10f702b92633666219af462e388d6a32c3386d78
WalScience <walid.boyboy@hotmail.fr>
2016-03-14 18:41:22 +0100

    Added files via upload

--- BT_Temperature_et_humidit_.ino
+++ BT_Temperature_et_humidit_.ino
@@ -1,13 +1,32 @@
+#include <dht11.h> // charger la librairie
 
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+dht11 DHT11; // Créer le capteur
+
+#define DHT11PIN 2 // Définir la pin utilisée
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC06(11,10);
+
+
+void setup()
+{
+  HC06.begin(9600); 
+}
+void loop()
+{
+    //while(HC06.available())
+    //{
+      
+  int chk = DHT11.read(DHT11PIN); // Lecture du capteur
+
+  HC06.print("Humidite (%): "); // Humidité
+  HC06.println((float)DHT11.humidity, 2);
+
+  HC06.print("Temperature (°C): "); // Température
+  HC06.println((float)DHT11.temperature, 2);
+
+  delay(1000); // Pause avant prochaine lecture
+    //}
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
-}
